34039980ae0c806553e12b9225dcfc07a0786f16
Ratmir Miftachov <40966507+ratmir-miftachov@users.noreply.github.com>
2024-11-23 15:54:53 -0500

    multivariate clustering is ready

--- clusters/multivariate_clustering.py
+++ clusters/multivariate_clustering.py
@@ -15,55 +15,48 @@ from scipy.spatial import distance_matrix
 import matplotlib.pyplot as plt
 from cycler import cycler
 
+# =======================================================
+# Load the Q vectors and find common columns
+# =======================================================
+
 # Get the default property cycle
 default_colors = plt.rcParams['axes.prop_cycle'].by_key()['color']
-
 # Set the first three colors to orange, blue, and red, respectively
 new_colors = ['#000000', '#0000FF', '#FF0000'] + default_colors[3:]
-
 # Set the new property cycle
 plt.rcParams['axes.prop_cycle'] = cycler(color=new_colors)
-
-
 #path="/Users/ratmir/Desktop/Clustering Zijin New"
 path="/Users/ratmir/Desktop/Clustering Zijin"
-
 os.chdir(path)
-
-# Load the .csv files into Pandas DataFrames
+# Load the Q vectors from the four different time-to-maturity (ttm) periods
 df1 = pd.read_csv('Q_matrix_5day_0d15.csv') # delete 20180610, 20181118
-#df1 = df1.drop(['20180610', '20181118'], axis=1)
 df2 = pd.read_csv('Q_matrix_9day_0d15.csv')
 df3 = pd.read_csv('Q_matrix_14day_0d15.csv')
 df4 = pd.read_csv('Q_matrix_27day_0d15.csv')
-
 # Get the column names from each DataFrame
 columns1 = list(df1.columns)[1:]
 columns2 = list(df2.columns)[1:]
 columns3 = list(df3.columns)[1:]
 columns4 = list(df4.columns)[1:]
-
 # Find common columns, preserving the order based on the first DataFrame
 common_columns = [col for col in columns1 if col in columns2 and col in columns3 and col in columns4]
-
 # Print all column lists and the common columns
 print("Common date columns:", common_columns)
 
-
-#Transformation
+# =======================================================
+# CLR Transformation as the paper: log(x) - log(gmean(x))
+# =======================================================
 def clr(x):
    return np.log(x) - np.log(gmean(x))
-
 df1_clr = clr(df1.drop(['Return'], axis=1))
 df2_clr = clr(df2.drop(['Return'], axis=1))
 df3_clr = clr(df3.drop(['Return'], axis=1))
 df4_clr = clr(df4.drop(['Return'], axis=1))
 
-
-
-# Initialize a list to hold the concatenated vectors
+# =====================================================================================================================
+# Calculate the pairwise multivariate Euclidean distances between the rows of this new matrix. Do the clustering.
+# =====================================================================================================================
 concatenated_vectors = []
-
 # Loop through each common date
 for date in common_columns:
    # Extract the columns (returns for the given date) from each DataFrame
@@ -78,20 +71,18 @@ for date in common_columns:
 
    # Append this concatenated vector to the list
    concatenated_vectors.append(concatenated_vector)
-
-# Convert the list of concatenated vectors to a NumPy array
 concatenated_matrix = np.array(concatenated_vectors)
-# Calculate the pairwise multivariate Euclidean distances between the rows of this new matrix
 distance_matrix_flat = scipy.spatial.distance.pdist(concatenated_matrix, 'euclidean')
-
-
 Z = sch.linkage(distance_matrix_flat, method='ward')
+cluster_labels = cut_tree(Z, height=35).T
+
+# =======================
+# Plot the dendrogram
+# =======================
 dendrogram_data = sch.dendrogram(Z)
 plt.xticks([])
-
 # Get the x-coordinates of the leaves (i.e., the original observations)
 leaf_x_positions = dendrogram_data['icoord']
-
 # Calculate mean x-position for each cluster's leaves to find a representative x-position for the cluster label
 mean_x_positions = [np.mean(xs) for xs in leaf_x_positions]
 cluster_0_x_pos = np.mean(mean_x_positions[:len(mean_x_positions)//2])
@@ -100,12 +91,14 @@ cluster_1_x_pos = np.mean(mean_x_positions[len(mean_x_positions)//2:])
 plt.text(x=cluster_0_x_pos, y=-5, s="HV Cluster", ha='center', va='center', fontsize=12, color='blue')
 plt.text(x=cluster_1_x_pos, y=-5, s="LV Cluster ", ha='center', va='center', fontsize=12, color='red')
 plt.show()
-
 ss= cut_tree(Z, n_clusters = 2).T
-
-cluster_labels = cut_tree(Z, height=35).T
-
 np.unique(ss, return_counts=True)
+
+
+
+# =======================================
+# PCA ANALSIS OF THE DISTANCE MATRIX
+# =======================================
 
 from scipy.spatial.distance import pdist, squareform
 from sklearn.decomposition import PCA
@@ -118,12 +111,11 @@ X_scaled = scaler.fit_transform(dist_matrix)
 # Step 2: Apply PCA
 pca = PCA(n_components=2)  # Reduce to 2 dimensions
 X_pca = pca.fit_transform(X_scaled)
-cluster_labels = ss.flatten()
+
 
 # Plotting the 2D principal components
 plt.figure(figsize=(8, 6))
 colors = ['blue', 'red']  # Define colors for each cluster
-
 labels = ['HV Cluster', 'LV Cluster']  # Custom labels for each cluster
 
 for i in range(len(X_pca)):
@@ -142,20 +134,17 @@ plt.show()
 
 
 
-
-######Plot for Q_alL, common columns:
+# =======================================================
+# Plot the common Q vectors for each ttm, both clusters
+# =======================================================
 df1_clr_com = df1_clr[common_columns]
 df2_clr_com = df2_clr[common_columns]
 df3_clr_com = df3_clr[common_columns]
 df4_clr_com = df4_clr[common_columns]
 returns = df1['Return']
-
-#all Q
 fig, axes = plt.subplots(2, 2, figsize=(10, 10))
-
 data_frames = [df1_clr_com, df2_clr_com, df3_clr_com, df4_clr_com]
 titles = ['ttm=5', 'ttm=9', 'ttm=14', 'ttm=27']
-
 for i, (df, title) in enumerate(zip(data_frames, titles)):
     ax = axes[i//2, i%2]
     ax.plot(returns, df, color='black', linewidth=0.5, alpha=0.1)
@@ -165,64 +154,47 @@ for i, (df, title) in enumerate(zip(data_frames, titles)):
 
     # Set y-axis limits
     ax.set_ylim(-3, 2.3)
-
-
-# Adjust layout to prevent labels from overlapping
 plt.tight_layout()
 plt.show()
 
-#Q_0
+# =======================================================
+# Plot the common Q vectors for each ttm, Q_0 cluster
+# =======================================================
 # Find the indices of the columns where the binary array has a 1
 zero_indices = np.where(ss == 1)[1]
-
 # Select only the columns corresponding to the zero indices
 selected_columns = [common_columns[i] for i in zero_indices if i < len(common_columns)]
-
-
 fig, axes = plt.subplots(2, 2, figsize=(10, 10))
-
 data_frames = [df1_clr_com, df2_clr_com, df3_clr_com, df4_clr_com]
 titles = ['ttm=5', 'ttm=9', 'ttm=14', 'ttm=27']
-
 for i, (df, title) in enumerate(zip(data_frames, titles)):
     ax = axes[i//2, i%2]
     ax.plot(returns, df[selected_columns], color='blue', linewidth=0.5, alpha=0.1)
     ax.set_xlabel('Returns')
     ax.set_ylabel('Q')
     ax.set_title(title)
-
     ax.set_ylim(-3, 2.3)
-
-
-# Adjust layout to prevent labels from overlapping
 plt.tight_layout()
 plt.show()
 
 
 
-#Q_1
-# Find the indices of the columns where the binary array has a 1
+# =======================================================
+# Plot the common Q vectors for each ttm, Q_1 cluster
+# =======================================================
+# Find the indices of the columns where the binary array has a 0
 zero_indices = np.where(ss == 0)[1]
-
 # Select only the columns corresponding to the zero indices
 selected_columns = [common_columns[i] for i in zero_indices if i < len(common_columns)]
-
-
 fig, axes = plt.subplots(2, 2, figsize=(10, 10))
-
 data_frames = [df1_clr_com, df2_clr_com, df3_clr_com, df4_clr_com]
 titles = ['ttm=5', 'ttm=9', 'ttm=14', 'ttm=27']
-
 for i, (df, title) in enumerate(zip(data_frames, titles)):
     ax = axes[i//2, i%2]
     ax.plot(returns, df[selected_columns], color='red', linewidth=0.5, alpha=0.1)
     ax.set_xlabel('Returns')
     ax.set_ylabel('Q')
     ax.set_title(title)
-
     ax.set_ylim(-3, 2.3)
-
-
-# Adjust layout to prevent labels from overlapping
 plt.tight_layout()
 plt.show()
